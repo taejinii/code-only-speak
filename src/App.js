8b4703251f054e303b0760e6df877d10d9344a07
taejinii <xowls177@gmail.com>
2023-02-05 06:55:48 +0900

    chore:데브 브랜치 만들기

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold">커밋테스트</h1>
+      <h1 className="text-3xl font-bold">데브 브랜치만들기</h1>
     </div>
   );
 }
