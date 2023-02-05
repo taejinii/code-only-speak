0a9f10134899aeaa008f29a590d36af96706be88
taejinii <xowls177@gmail.com>
2023-02-05 20:37:11 +0900

    feat:OPEN_AI 서버파일 추가 및 작동테스트

---
+++ server.js
@@ -0,0 +1,47 @@
+const OpenAi = require("openai");
+const { Configuration, OpenAIApi } = OpenAi;
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const port = 5001;
+const app = express();
+
+const configuration = new Configuration({
+  organization: "org-eyG5tfsktdibhPnjUoitjURd",
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    message: "Hello OPENAI",
+  });
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const { message } = req.body;
+    // console.log("message", message);
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${message}`,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+      temperature: 0,
+    });
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening`);
+});
--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import "./App.css";
-
+import { useState } from "react";
 function App() {
+  const [message, setMessage] = useState("");
+  const [response, setResponse] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:5001/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setResponse(data.bot);
+      });
+  };
+  console.log(message);
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold">데브 브랜치만들기</h1>
+    <div className="flex flex-col justify-center items-center">
+      <form onSubmit={handleSubmit}>
+        <textarea
+          className="border-2 border-black"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+        <button type="submit" className="border-2">
+          Submit
+        </button>
+      </form>
+      <div>{response}</div>
     </div>
   );
 }
-
 export default App;
